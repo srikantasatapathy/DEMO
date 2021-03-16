5b677707049a578f6c3b134cd0dd49b85ee38dcc
LENOVO <LENOVO@LAPTOP-7KG4MU5S>
2021-03-16 11:36:06 +0530

    Add 2 message

--- Demo/src/Demo.java
+++ Demo/src/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo project");
+		System.out.println("Hiiii");
+		System.out.println("Helo");
 	}
 
 }
